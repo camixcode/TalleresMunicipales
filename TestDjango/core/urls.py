f27702291be3263a51cb042b3a7bea6f7b7aec1a
Cami <101901288+camixcode@users.noreply.github.com>
2022-08-18 16:15:55 -0400

    muchas cosas

--- TestDjango/core/urls.py
+++ TestDjango/core/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from.views import home
+from.views import home, Form_Inscripcion_Taller, Form_Instructor_Taller, Navbar
 
 urlpatterns = [
-    path('', home, name="home"),
+    path('home', home, name="home"),
+    path('Form_Inscripcion_Taller/', Form_Inscripcion_Taller, name="Formulario_Inscripcion_Taller"),
+    path('Form_Instructor_Taller/', Form_Instructor_Taller, name="Formulario_Instructor_Taller"),
+    path('Navbar/', Navbar, name="Navbar"),
 ]
--- TestDjango/core/views.py
+++ TestDjango/core/views.py
@@ -3,3 +3,13 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request,'core/home.html')
+
+
+def Form_Inscripcion_Taller(request):
+    return render(request, 'core/Form_Inscripcion_Taller.html')
+
+def Form_Instructor_Taller(request):
+    return render(request, 'core/Form_Instructor_Taller.html')    
+
+def Navbar(request):
+    return render(request, 'core/Navbar.html')    
