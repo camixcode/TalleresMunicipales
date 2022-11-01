916c75f9e495c74232d9a970cc99f47dc965a754
AlexD30 <yer.vasquez@duocuc.cl>
2022-11-01 11:09:35 -0300

    Base

--- TestDjango/core/urls.py
+++ TestDjango/core/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from.views import home, Form_Inscripcion_Taller, Form_Instructor_Taller, Navbar
+from.views import Admin_Taller, home, Form_Inscripcion_Taller, Form_Instructor_Taller,Ins_Taller
 
 urlpatterns = [
-    path('home', home, name="home"),
-    path('Form_Inscripcion_Taller/', Form_Inscripcion_Taller, name="Formulario_Inscripcion_Taller"),
-    path('Form_Instructor_Taller/', Form_Instructor_Taller, name="Formulario_Instructor_Taller"),
-    path('Navbar/', Navbar, name="Navbar"),
+    path('', home, name="home"),
+    path('Form_Inscripcion_Taller/', Form_Inscripcion_Taller, name="Form_Inscripcion_Taller"),
+    path('Form_Instructor_Taller/', Form_Instructor_Taller, name="Form_Instructor_Taller"),
+    path('PostularTaller/',Ins_Taller, name="Ins_Taller"),
+    path('AdministradorTaller/',Admin_Taller, name="Admin_Taller"),
+    
 ]
--- TestDjango/core/views.py
+++ TestDjango/core/views.py
@@ -4,12 +4,14 @@ from django.shortcuts import render
 def home(request):
     return render(request,'core/home.html')
 
-
 def Form_Inscripcion_Taller(request):
     return render(request, 'core/Form_Inscripcion_Taller.html')
 
 def Form_Instructor_Taller(request):
     return render(request, 'core/Form_Instructor_Taller.html')    
+ 
+def Ins_Taller(request):
+    return render (request, 'core/Ins_Taller.html')
 
-def Navbar(request):
-    return render(request, 'core/Navbar.html')    
+def Admin_Taller (request):
+    return render (request, 'core/Admin_Taller.html')    
