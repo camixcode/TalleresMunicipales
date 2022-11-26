c5ecd0d6ade0def9ab85373ae82ed4d7c78f965d
psicodelicko <101906675+psicodelicko@users.noreply.github.com>
2022-11-25 23:09:08 -0300

    Base de datos

--- TestDjango/core/models.py
+++ TestDjango/core/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Material(models.Model):
+    idMaterial = models.AutoField(primary_key=True, verbose_name='Id de material')
+    nombreMaterial = models.CharField(max_length=50, verbose_name='Nombre de material')
+    descripcionMaterial = models.CharField(max_length=50, verbose_name='Descripcion de material')
+    stock = models.IntegerField(verbose_name='Stock de material')
+
+    def __str__(self):
+        return self.nombreMaterial
