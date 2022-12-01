a7095bc9e401aaf3c768141f95bf1ac7b417eed7
psicodelicko <101906675+psicodelicko@users.noreply.github.com>
2022-12-01 03:25:32 -0300

    comit

--- TestDjango/TestDjango/urls.py
+++ TestDjango/TestDjango/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+#from django.conf.urls.static import static
+#from django.conf.urls import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
+
 ]
--- TestDjango/core/form.py
+++ TestDjango/core/form.py
@@ -1,5 +1,8 @@
 from django import forms
 from .models import Material
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from django.forms import ModelForm
 
 
 class MaterialForm (forms.ModelForm):
@@ -7,3 +10,24 @@ class MaterialForm (forms.ModelForm):
     class Meta:
         model = Material 
         fields = ("idMaterial","nombreMaterial","descripcionMaterial","stock")
+
+
+    def clean(self):
+        print(self.cleaned_data)
+        return self.cleaned_data
+
+
+class CustomerUserCreationForm (UserCreationForm):
+   class Meta:
+    model = User
+    fields=['username',"first_name","last_name","email","password1","password2"]
+
+class ModificarUsuario (ModelForm):
+   class Meta:
+    model = User
+    fields=['username',"first_name","last_name","email"]
+
+class CrearCuentaAdmin (UserCreationForm):
+   class Meta:
+    model = User
+    fields=['username',"first_name","last_name","email","is_superuser","password1","password2"]
